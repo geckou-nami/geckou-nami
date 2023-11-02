7d0d62b56d3d80a39857854ecf28fb3459ceb8f4
吉岡奈美 <nami@yoshiokaminoAir.elecom>
2023-11-02 11:16:46 +0900

    環境設定構築中

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app:{
+    head: {
+      charset:'utf-8',
+      title: "PRACTICE_JLCA",
+      meta: [
+        {name: "viewport",content:"width=device-width, initial-scale=1.0"},
+        {name: "description",content:"日本ライブコマース協会です"},
+        { name: 'format-detection', content: 'telephone=no' },
+      ],
+    },
+  },
+  components: {
+    global: true,
+    dirs  : ['~/components'],
+  },
+  css: [
+    '@acab/reset.css',
+    '~/assets/scss/base.scss',
+  ],
 })
